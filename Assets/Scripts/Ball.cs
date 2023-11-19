e4eaac2a63e3a52119f5667da5374dbc3005bf45
Pukkkkk <jidapa.petc@bumail.net>
2023-11-19 22:48:23 +0700

    Ball Script

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BallColor
+{
+    White, Red, Yellow, Green, Brown, Blue, Pink, Black
+}
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField]
+    private int point;
+    public int Point {  get { return point; } }
+
+    [SerializeField]
+    private BallColor ballcolor;
+
+    [SerializeField]
+    private MeshRenderer rd;
+
+    void Awake()
+    {
+        rd = GetComponent<MeshRenderer>();
+    }
+    
+    public void SetColorAndPoint(BallColor col)
+    {
+        switch(col)
+        {
+            case BallColor.White:
+                point = 0;
+                rd.material.color = Color.white;
+                break;
+            case BallColor.Red:
+                point = 1;
+                rd.material.color = Color.red;
+                break;
+            case BallColor.Yellow:
+                point = 2;
+                rd.material.color = Color.yellow;
+                break;
+            case BallColor.Green:
+                point = 3;
+                rd.material.color = Color.green;
+                break;
+            case BallColor.Brown:
+                point = 4;
+                rd.material.color = new Color32(255, 123, 0, 255);
+                break;
+            case BallColor.Blue:
+                point = 5;
+                rd.material.color = Color.blue;
+                break;
+            case BallColor.Pink:
+                point = 6;
+                rd.material.color = new Color32(255, 105, 224, 255);
+                break;
+            case BallColor.Black:
+                point = 7;
+                rd.material.color = Color.blue;
+                break;
+        }
+    }
+}
+
